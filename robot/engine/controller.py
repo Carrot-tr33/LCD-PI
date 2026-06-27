6111c965c6465c10af2a437b662656f88ae1c4aa
ST10453757 <161840799+ST10453757@users.noreply.github.com>
2026-06-27 11:25:43 +0200

    bug fix

--- robot/engine/controller.py
+++ robot/engine/controller.py
@@ -1,4 +1,5 @@
 from robot.core import ST7789
+import time
 
 class HardwareController:
     def __init__(self, disp):
@@ -11,16 +12,20 @@ class HardwareController:
         Returns the corresponding emotion string if pressed, otherwise None.
         """
         # Active-low buttons: 0 means the button is physically pressed down
-        if self.disp.digital_read(self.disp.GPIO_KEY1_PIN) == 0:        # KEY1 / Button A
+        if self.disp.digital_read(self.disp.GPIO_KEY1_PIN) == 0:       # KEY1 / Button A
+            time.sleep(0.15)        
             return "happy"
             
         elif self.disp.digital_read(self.disp.GPIO_KEY2_PIN) == 0:      # KEY2 / Button B
+            time.sleep(0.15)
             return "angry"
             
         elif self.disp.digital_read(self.disp.GPIO_KEY3_PIN) == 0:      # KEY3 / Button C
+            time.sleep(0.15)
             return "gold"
             
         elif self.disp.digital_read(self.disp.GPIO_KEY_PRESS_PIN) == 0: # Joystick Center Press
+            time.sleep(0.15)
             return "neutral"
             
         return None
